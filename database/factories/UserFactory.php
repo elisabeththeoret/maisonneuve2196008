ea06929051e36e1b0b30039db9dbe34e7ba4083e
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2023-03-07 12:46:19 -0500

    Correction, constance du code...

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,15 +5,14 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-class UserFactory extends Factory
-{
+class UserFactory extends Factory {
+
     /**
      * Define the model's default state.
      *
      * @return array
      */
-    public function definition()
-    {
+    public function definition() {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
@@ -28,12 +27,12 @@ class UserFactory extends Factory
      *
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified()
-    {
+    public function unverified() {
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
             ];
         });
     }
+
 }
