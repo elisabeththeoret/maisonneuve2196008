e7f1785c223d9c339eebb15f95bfc03bd9ab34d7
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2023-03-10 16:26:28 -0500

    Creation du controller 'Home'

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Home 
+ */
+class HomeController extends Controller {
+
+    /**
+     * Display the homepage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        // 
+    }
+
+}
