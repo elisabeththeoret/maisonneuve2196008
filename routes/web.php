fb47420ae3bcbc2c2158e893e3910790555dc161
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2023-03-10 19:10:21 -0500

    Creation des routes pour 'Home' et 'Etudiant'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EtudiantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Home 
+ */
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', function () {
+    return redirect(
+        route('home')
+    );
 });
+
+/**
+ * Etudiant 
+ */
+Route::get('/etudiant', [EtudiantController::class, 'index'])->name('etudiant');
+Route::get('/etudiant-create', [EtudiantController::class, 'create'])->name('etudiant.create');
+Route::post('/etudiant-create', [EtudiantController::class, 'store'])->name('etudiant.store');
+Route::get('/etudiant-show/{etudiant}', [EtudiantController::class, 'show'])->name('etudiant.show');
+Route::get('/etudiant-edit/{etudiant}', [EtudiantController::class, 'edit'])->name('etudiant.edit');
+Route::put('/etudiant-edit/{etudiant}', [EtudiantController::class, 'update'])->name('etudiant.update');
+Route::delete('/etudiant-edit/{etudiant}', [EtudiantController::class, 'destroy'])->name('etudiant.delete');
